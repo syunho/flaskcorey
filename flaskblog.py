d6ff381cd651cb98b390449316f5cf48554b64f6
syunho <gentoparliament@yahoo.co.jp>
2018-05-10 06:56:25 +0900

    Part2 6:51

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'syunho',
+        'title': 'Blog Post1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'HIRO',
+        'title': 'Blog Post2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
